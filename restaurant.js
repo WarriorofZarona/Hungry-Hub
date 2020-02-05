fd9b7760825de196eba776f1e31e3bb1e24cabfb
Hector Agudelo <haacny86@gmail.com>
2020-02-05 12:52:17 -0500

    test restaurant card made. In need of some make-up to make it look better, and probably adding the restaurant address could be a good idea.

--- restaurant.js
+++ restaurant.js
@@ -1,5 +1,8 @@
+
 var cityInput = "New York City"
+
 var cityURL = "https://developers.zomato.com/api/v2.1/cities?q=" + cityInput;
+
 $.ajax({
     url: cityURL,
     method: "GET",
@@ -7,10 +10,13 @@ $.ajax({
         request.setRequestHeader("user-key", "173297606dd309e858d947e8c0e0562c");
     },
 }).then(function (response) {
+
     console.log(response.location_suggestions[0].id);
     var cityId = response.location_suggestions[0].id
-    var searchURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityId + "&entity_type=city"
+    var searchURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityId + "&entity_type=city";
     console.log(searchURL);
+
+
     $.ajax({
         url: searchURL,
         method: "GET",
@@ -19,5 +25,28 @@ $.ajax({
         },
     }).then(function (result) {
         console.log(result);
-    })
+        var card_title = result.restaurants[0].restaurant.name;
+        console.log(card_title);
+        $(".card-title").text(card_title);
+
+        var resLink = result.restaurants[0].restaurant.url;
+        console.log(resLink);
+        $('a').attr("target", "_blank");
+        $('a').attr("href", resLink).text('For more Information Click Here');
+
+        var resImg = result.restaurants[0].restaurant.thumb;
+        console.log(resImg);
+        $('img').addClass('response_img').attr('src', resImg).attr('alt', 'Restaurant Image');
+
+
+        var resRatingNum = result.restaurants[0].restaurant.user_rating.aggregate_rating;
+        var resRatingStr = result.restaurants[0].restaurant.user_rating.rating_text;
+        console.log(resRatingNum, resRatingStr);
+        $('p').addClass('rating');
+
+        $('.rating').empty().text("Restaurant Rating: " + resRatingNum + " " + resRatingStr).css('text-align', 'left');
+
+
+    });
+
 });
