34e555dd4bbd3c60e95077464c9a09829bb742eb
Andrew Bautista <andrewbautista85@gmail.com>
2020-02-04 19:10:48 -0500

    slowly building card result, have image and card title, but takes up full screen

--- recipe.js
+++ recipe.js
@@ -3,6 +3,13 @@ var keyWord = "chicken" //location of user input for recipe
 
 var queryURL = "https://api.edamam.com/search?app_id=2d10e9e9&app_key=041becfbb0cfe254d9b264eb2339c614&q=" + keyWord;
 
+var createContainer = $("<div>").attr("id", "container");
+
+$("body").append(createContainer);
+
+var containerId = $("#container");
+
+
 $.ajax({
 
     url: queryURL,
@@ -30,14 +37,22 @@ $.ajax({
 
 
     // })
+    // Create card div
+    var createCard = $("<div>").addClass("card small");
+    var createCardImg = $("<div>").addClass("card-image")
+
     // Use response.hits[0], using for loop to go through each result
     var result = response.hits[0]
-    // This should display recipe name
-    // console.log("The recipe name is " + response.hits[0].recipe.label)
-    $("#recipe0").text(result.recipe.label)
+
     // This should display recipe image, to be used in img src
     // console.log("Image source is " + response.hits[0].recipe.image)
-    $("#image0").addClass("responsive-img").attr("src", result.recipe.image)
+    createCardImg.append($("<img>").addClass("responsive-img").attr("src", result.recipe.image))
+    // This should display recipe name
+    // console.log("The recipe name is " + response.hits[0].recipe.label)
+    createCardImg.append($("<span>").addClass("card-title").text(result.recipe.label))
+
+    createCard.append(createCardImg);
+
     // This should display source of recipe, to be anchored by url
     // console.log("Source is " + response.hits[0].recipe.source + " and url is " + response.hits[0].recipe.url)
     $("#link0").attr("href", result.recipe.url).text(result.recipe.source)
@@ -46,6 +61,8 @@ $.ajax({
     $("#results0").append($("<p>").addClass("valign-wrapper left").attr("id", "calorie0").text(Math.round(result.recipe.calories) + " calories"))
     // console.log("Number of ingredients: " + response.hits[0].recipe.ingredients.length)
     $("#results0").append($("<p>").addClass("valign-wrapper right").attr("id", "ingredients0").text(result.recipe.ingredients.length + " ingredients"))
+
+    containerId.append(createCard);
 })
 
 // Notes: Need to add ID's for Card Title (recipe0), Image (image0), Link insert (link0), and Card Content (result0)
