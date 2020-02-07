4cbe01cecdaade1e90e9d88302969e6bfac20790
Hector Agudelo <haacny86@gmail.com>
2020-02-06 20:51:09 -0500

    restaurant web displaying the restaurants

--- restaurant.js
+++ restaurant.js
@@ -1,7 +1,7 @@
 
-var cityInput = "New York City"
+var cityInput = "chicago";
 
-var cityURL = "https://developers.zomato.com/api/v2.1/cities?q=" + cityInput;
+var cityURL = "https://developers.zomato.com/api/v2.1/cities?count=12&q=" + cityInput;
 
 $.ajax({
     url: cityURL,
@@ -13,7 +13,7 @@ $.ajax({
 
     console.log(response.location_suggestions[0].id);
     var cityId = response.location_suggestions[0].id
-    var searchURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityId + "&entity_type=city";
+    var searchURL = "https://developers.zomato.com/api/v2.1/search?count=8&entity_id=" + cityId + "&entity_type=city";
     console.log(searchURL);
 
 
@@ -24,27 +24,53 @@ $.ajax({
             request.setRequestHeader("user-key", "173297606dd309e858d947e8c0e0562c");
         },
     }).then(function (result) {
+
         console.log(result);
-        var card_title = result.restaurants[0].restaurant.name;
-        console.log(card_title);
-        $(".card-title").text(card_title);
+        for (var i = 0; i < result.restaurants.length; i++) {
 
-        var resLink = result.restaurants[0].restaurant.url;
-        console.log(resLink);
-        $('a').attr("target", "_blank");
-        $('a').attr("href", resLink).text('For more Information Click Here');
+            var createCardContainer = $("<div>").addClass("col s12 l3").attr("id", "card" + i);
+            $("#results").append(createCardContainer);
+            var containerId = $("#card" + i);
+            var createCard = $("<div>").addClass("card large");
+            var createCardImg = $("<div>").addClass("card-image");
+            var createCardContent = $("<div>").addClass("card-content");
+            var createCardAction = $("<div>").addClass("card-action");
 
-        var resImg = result.restaurants[0].restaurant.thumb;
-        console.log(resImg);
-        $('img').addClass('response_img').attr('src', resImg).attr('alt', 'Restaurant Image');
+            var results = result.restaurants[i];
+
+            createCardImg.append($("<img>").addClass("responsive-image").attr("src", results.restaurant.thumb))
+
+            createCardImg.append($("<span>").addClass("card-title").text(results.restaurant.name))
+
+            console.log(createCardImg)
+
+            createCard.append(createCardImg);
+
+            var resRatingNum = results.restaurant.user_rating.aggregate_rating;
+            var resRatingStr = results.restaurant.user_rating.rating_text;
+            createCardContent.append($("<p>").addClass("resRating").text("Restaurant Rating: " + resRatingNum + " " + resRatingStr).css('text-align', 'left'))
+            var resAddr = results.restaurant.location.address;
+            var resPhone = results.restaurant.phone_numbers;
+            createCardContent.append($("<p>").addClass("resInfo").html('Address: ' + resAddr + '<br>' + 'Telephone Number: ' + resPhone))
+            createCard.append(createCardContent);
+
+            var resLink = results.restaurant.url;
+            createCardAction.append($("<a>").attr("href", resLink).text('See More').attr("target", "_blank"))
+
+            createCard.append(createCardAction);
+
+            containerId.append(createCard);
 
 
-        var resRatingNum = result.restaurants[0].restaurant.user_rating.aggregate_rating;
-        var resRatingStr = result.restaurants[0].restaurant.user_rating.rating_text;
-        console.log(resRatingNum, resRatingStr);
-        $('p').addClass('rating');
 
-        $('.rating').empty().text("Restaurant Rating: " + resRatingNum + " " + resRatingStr).css('text-align', 'left');
+
+
+
+
+
+
+
+        }
 
 
     });
